ec88b96b3a30e99dcc83e7846dea512907ae28d9
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-18 22:37:43 -0600

    added floating button

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,34 @@ export default class App extends React.Component {
             <div>
                 <Navbar /> 
                 <Route exact path="/" component={Carousel} /> 
+            
+                <div class='fixed-action-btn horizontal'> 
+                    <a class='btn-floating btn-large red'>
+                      <i class='material-icons'>mode_edit</i>
+                    </a>
+                    <ul>
+                      <li>
+                        <a class='btn-floating red'>
+                          <i class='material-icons'>insert_chart</i>
+                        </a>
+                      </li>
+                      <li>
+                        <a class='btn-floating yellow'>
+                          <i class='material-icons'>publish</i>
+                        </a>
+                      </li>
+                      <li>
+                        <a class='btn-floating green'>
+                          <i class='material-icons'>attach_file</i>
+                        </a>
+                      </li>
+                      <li>
+                        <a class='btn-floating blue'>
+                          <i></i>
+                        </a>
+                      </li>
+                    </ul>
+                </div>
             </div>
         );
     }
