931c458c43efee166294fb286c20660b1c89f178
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-19 23:19:12 -0600

    added state and form content for login

--- client/src/components/forms/login.js
+++ client/src/components/forms/login.js
@@ -4,10 +4,42 @@ import '../styles/forms/login.css'
 export default class Login extends React.Component{
     constructor(){
         super()
+        this.state = {
+            email: '',
+            password: ''
+        }
     }
+
+    changeHandler = (event) => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+        console.log(this.state.email, this.state.password)
+    }
+
     render(){
         return(
-            <div>Login</div> 
+            <form onClick={this.submitHandler}>
+                <input type='text'
+                       value={this.state.email}
+                       name='email'
+                       placeholder='Email'
+                       onChange={this.changeHandler}
+                />
+
+                <input type='password'
+                       value={this.state.password}
+                       placeholder='Password'
+                       onChange={this.changeHandler}
+                       name='password'
+                /> 
+                <button type='submit'>Submit</button>
+            </form>  
         )
     }
 }
