a9f149084801c5a6a547c4bd9d1cab6f44a69200
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-19 22:23:47 -0600

    connected newly created files to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,46 +3,26 @@ import './App.css';
 
 import Navbar from './components/nav/navbar.js'
 import Carousel from './components/carousel/carousel.js'
+import Login from './components/forms/login'
+import Register from './components/forms/register'
+import PlantHangerCards from './components/plantHangers/plantHangerCards'
+import WallHangerCards from './components/wallHangers/wallHangerCards'
+
 import {Route} from 'react-router-dom'
 
 
 export default class App extends React.Component {
-
-
 
     render(){
         return(
             <div>
                 <Navbar /> 
                 <Route exact path="/" component={Carousel} /> 
-            
-                <div class='fixed-action-btn horizontal'> 
-                    <a class='btn-floating btn-large red'>
-                      <i class='material-icons'>mode_edit</i>
-                    </a>
-                    <ul>
-                      <li>
-                        <a class='btn-floating red'>
-                          <i class='material-icons'>insert_chart</i>
-                        </a>
-                      </li>
-                      <li>
-                        <a class='btn-floating yellow'>
-                          <i class='material-icons'>publish</i>
-                        </a>
-                      </li>
-                      <li>
-                        <a class='btn-floating green'>
-                          <i class='material-icons'>attach_file</i>
-                        </a>
-                      </li>
-                      <li>
-                        <a class='btn-floating blue'>
-                          <i></i>
-                        </a>
-                      </li>
-                    </ul>
-                </div>
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/landing' component={Landing}/>
+                <Route exact path='/PlantHangerCards' component={PlantHangerCards} /> 
+                <Route exact path='/WallHangerCards' component={WallHangerCards} /> 
             </div>
         );
     }
