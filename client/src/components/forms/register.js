e6ca43c8299275aafd0fb3d71d75f024499086cc
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-19 23:56:17 -0600

    added inputs, state, and handlers to register form

--- client/src/components/forms/register.js
+++ client/src/components/forms/register.js
@@ -4,10 +4,48 @@ import '../styles/forms/register.css'
 export default class Register extends React.Component{
     constructor(){
         super()
+        this.state = {
+            email: '',
+            username: '',
+            password: ''
+        }
     }
+
+    changeHandler = (event) => {
+        event.preventDefault()
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+        console.log(this.state.email, this.state.password, this.state.username)
+    }
+
     render(){
         return(
-            <div>Register</div> 
+            <form onClick={this.submitHandler}>
+                <input type='text'
+                       value={this.state.email}
+                       name='email'
+                       placeholder='Email'
+                       onChange={this.changeHandler}
+                />
+                <input type='username'
+                       value={this.state.username}
+                       placeholder='Username'
+                       onChange={this.changeHandler}
+                       name='username'
+                /> 
+                <input type='password'
+                       value={this.state.password}
+                       placeholder='Password'
+                       onChange={this.changeHandler}
+                       name='password'
+                /> 
+                <button type='submit'>Submit</button>
+            </form>
         )
     }
 }
