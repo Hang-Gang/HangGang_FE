ff87ce595c161b95dbe25be42d03accb13b60d28
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2020-02-19 22:10:13 -0600

    added style folders for forms and landing, added folders, files, and boilerplate for planthangers and wallhangers

---
+++ client/src/components/plantHangers/plantHangerCard.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+
+export default class PlantHangerCards extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>PlantHangerCards</div> 
+        )
+    }
+}
---
+++ client/src/components/wallHangers/wallHangerCard.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+
+
+export default class WallHangerCard extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <div>WallHangerCard</div> 
+        )
+    }
+}
